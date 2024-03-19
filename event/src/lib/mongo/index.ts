c8d1eaf670448d328547619020c46b25d1d7af4d
jeisaraja <jeisa.raja.jrt@gmail.com>
2024-03-20 00:42:40 +0700

    feat: Set up database connection and user schema
    - Added database connection logic using Mongoose in 'lib/mongo/index.ts'
    - Defined User schema with fullname, email, username, password, and isSubscribed fields in 'models/User.ts'

---
+++ event/src/lib/mongo/index.ts
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+declare global {
+  var mongoose: any;
+}
+
+let uri: string
+
+if (process.env.MONGODB_URI) {
+  uri = process.env.MONGODB_URI
+} else {
+  throw new Error("Please define the MONGODB_URI environment variable inside .env.local")
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+const dbConnect = async () => {
+  if (cached.conn) {
+    return cached.conn
+  }
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    }
+    cached.promise = mongoose.connect(uri, opts).then((mongoose) => {
+      return mongoose
+    })
+  }
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null
+    throw e
+  }
+  return cached.conn
+}
+
+export default dbConnect
