3199d637cc1dba0cda9883401655ef624f721c3b
devavaz <135300758+devavaz@users.noreply.github.com>
2024-03-21 22:10:07 +0900

    Pair programming with BK

--- event/src/components/Header.tsx
+++ event/src/components/Header.tsx
@@ -3,7 +3,9 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header className="flex w-full justify-between bg-sky-100 px-6 py-4">
-      <h1>LOGO</h1>
+      <Link href="/" className="text-sky-700">
+        LOGO
+      </Link>
       <nav className="flex gap-4">
         <Link className="text-sky-700" href="/events">
           Events
