2fd97e6d4f4b4fbee9fee16747e0ac43421c35d0
bk7312 <bk7312@users.noreply.github.com>
2024-03-25 20:33:47 +0800

    Create event schema
    - Create event schema
    - Minor edits on user schema
    - Add Mongoose type on lib/mongo/index.ts

---
+++ event/src/models/Event.ts
@@ -0,0 +1,74 @@
+import mongoose, { Types, Schema } from "mongoose";
+
+export interface Events extends mongoose.Document {
+  name: string;
+  slug: string; // has to be unique, default to name.split(' ').join('-')?
+  description: string;
+  location: string;
+  imgPoster: string; // image url
+  category: string[];
+  eventStartDate: Date; // new Date().setUTCHours
+  eventEndDate?: Date;
+  lastDateToJoin: Date;
+  maximumParticipants: number;
+  host: Types.ObjectId;
+  participants: Types.ObjectId[];
+}
+
+const Eventschema = new mongoose.Schema<Events>({
+  name: {
+    type: String,
+    lowercase: true,
+    unique: true,
+    required: true,
+  },
+  slug: {
+    type: String,
+    minlength: [3, "Slug must be 3 letters long"],
+    unique: true,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  imgPoster: {
+    type: String,
+    default: "/placeholder/image",
+  },
+  category: {
+    type: [String],
+    default: ["Uncategorized"],
+  },
+  eventStartDate: {
+    type: Date,
+    required: true,
+  },
+  eventEndDate: {
+    type: Date,
+  },
+  lastDateToJoin: {
+    type: Date,
+    required: true,
+  },
+  maximumParticipants: {
+    type: Number,
+    default: 0, // 0 means no limit
+  },
+  host: {
+    type: Schema.Types.ObjectId,
+    ref: "users",
+    required: true,
+  },
+  participants: {
+    type: [Schema.Types.ObjectId],
+    ref: "users",
+  },
+});
+
+// export default mongoose.model<Events>('Events', Eventschema);
+export default mongoose.models.events || mongoose.model("events", Eventschema);
