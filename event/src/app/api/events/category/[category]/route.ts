2f293f43d8e458b1e506daef32ef88e154cab3c8
bk7312 <bk7312@users.noreply.github.com>
2024-03-31 21:24:44 +0800

    Fix category page error by reverting to dummy data

--- event/src/app/api/events/category/[category]/route.ts
+++ event/src/app/api/events/category/[category]/route.ts
@@ -7,18 +7,19 @@ import { NextRequest, NextResponse } from "next/server";
 // get event from mongo and sort into lists by category
 // send data to client
 
-export async function GET(request: NextRequest, { params }: { params: { category: string } }) {
-
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { category: string } },
+) {
   try {
-    console.log("GET REQUEST")
     await dbConnect();
-    
-    const events = await Event.find({ category: params.category })
+
+    const events = await Event.find({ category: params.category });
 
     if (!events || events.length === 0) {
       return NextResponse.json({ error: "No events in db" });
     }
-    return NextResponse.json({ message: "Successfully joined", data: events });
+    return NextResponse.json({ data: events });
   } catch (error) {
     const err = error as Error;
     console.log("error caught:", error);
--- event/src/app/category/page.tsx
+++ event/src/app/category/page.tsx
@@ -1,7 +1,8 @@
+import { CATEGORIES } from "@/lib/constants";
 import EventCard from "@/components/EventCard";
 import EventList from "@/components/EventList";
+import { getEventByCategory } from "@/lib/dummyBackend";
 import { EventType } from "@/lib/types";
-import { uuid } from 'uuidv4';
 
 type CategoryData = {
   id: string;
@@ -9,83 +10,45 @@ type CategoryData = {
   event: EventType[];
 }[];
 
-var hide = true;
-
 export default async function Events() {
   const categoryData: CategoryData = [];
-  const response = await fetch('http://localhost:3000/api/events/getEventsByCategory', {
-    method: 'GET',
-    headers: {
-      "Content-type": "application/json"
-    },
-  });
-  if (response.ok) {
-    const data = await response.json()
-    //console.log("response ok ", data.data)
-    const categories = data.data;
 
-    if (data.error) {
-      hide = false;
-    }
-    try {
-      for (const i in categories) {
-        //console.log(categories[i])
-        const newCat = {
-          id: uuid(),
-          category: categories[i]._id,
-          event: categories[i].documents
-        }
-        categoryData.push(newCat)
-      }
-      console.log(categoryData)
-      // for (const event of data.data) {
-      //   const categories = event.category;
-      //   for (const category of categories) {
-      //     const existingCategory = categoryData.find(cat => cat.category === category);
-      //     if (existingCategory) {
-      //       existingCategory.event.push(event);
-      //     } else {
-      //       const newCat = {
-      //         id: uuid(),
-      //         category: category,
-      //         event: [event]
-      //       }
-      //       categoryData.push(newCat)
-      //     }
+  for (let cat of CATEGORIES) {
+    const { data } = await getEventByCategory(cat);
 
-      //   }
-      // }
-    } catch (err) {
-      console.log(err)
+    if (!data) {
+      continue;
     }
-  } else {
-    hide = false;
+
+    categoryData.push({
+      id: crypto.randomUUID(),
+      category: cat,
+      event: data.slice(0, 3),
+    });
   }
+
+  console.log("catdata", categoryData);
   return (
     <>
       <div className="flex flex-col gap-2 bg-lime-200">
         <p className="text-4xl font-bold">Categories: </p>
-        {hide && categoryData.map((cat) => (
+        {categoryData.map((cat) => (
           <EventList category={cat.category} key={cat.id}>
-            {cat.event.map((event) => {
-              return (
-                <EventCard
-                  key={event.id}
-                  id={event.id}
-                  eventName={event.eventName}
-                  date={new Date(event.eventStartDate)}
-                  location={event.location}
-                  img={event.imgPoster}
-                  views={event.weeklyViews}
-                />
-              )
-            }
-            )}
+            {cat.event.map((event) => (
+              <EventCard
+                key={event.id}
+                id={event.id}
+                eventName={event.eventName}
+                date={event.date}
+                location={event.location}
+                img={event.img}
+                views={event.weeklyViews}
+              />
+            ))}
           </EventList>
-        )
-        )}
+        ))}
 
-        {!hide && (<EventList category="Test404Category">
+        <EventList category="Test404Category">
           <EventCard
             id="Testing404Event"
             eventName="Testing404Event"
@@ -94,7 +57,7 @@ export default async function Events() {
             img="https://picsum.photos/id/1/200/150"
             views={0}
           />
-        </EventList>)}
+        </EventList>
       </div>
     </>
   );
