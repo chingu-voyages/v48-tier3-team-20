7ff129aec31612066d6225765b29cad3dbff1aa5
Eric Iwan <eric.iwan@proton.me>
2024-04-02 03:54:57 +0000

    Fixed endpoint

--- event/src/app/api/users/isUserLoggedIn/route.ts
+++ event/src/app/api/users/isUserLoggedIn/route.ts
@@ -8,12 +8,12 @@ export default async function GET(req:Request){
 
     //body should be returning a token string I believe.
     const body = await req.json();
-
-    if(!body.token){
+    const token = body.cookies.get("accessToken");
+    if(!token){
         return NextResponse.json({message: "Not logged in"})
     }
     try{
-        const userData = await verifyJwt(body.token);
+        const userData = await verifyJwt(token);
         return NextResponse.json({message: "user verified", data: userData})
 
     } catch(err){
