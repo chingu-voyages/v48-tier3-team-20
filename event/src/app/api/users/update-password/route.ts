53d1dd996ef2f535cc322b0ddcc55732a078c7a3
Eric Iwan <eric.iwan@proton.me>
2024-04-05 21:43:49 +0900

    Update-password finished

--- event/src/app/api/users/update-password/route.ts
+++ event/src/app/api/users/update-password/route.ts
@@ -1,26 +1,23 @@
 import dbConnect from "@/lib/mongo/index";
-import Users, { IUsers } from "@/models/User";
-import { NextResponse } from "next/server";
+import User from "@/models/User";
+import { NextRequest, NextResponse } from "next/server";
 import { verifyJwt } from "@/lib/authHelper";
-import { jwtVerify } from "jose";
+import bcrypt from "bcrypt";
 
-
-//new password is being sent in req along with old password to verify. 
-//need to check that old password still matches the user.id in the cookie.
-//need to findOneandUpdate with new password.
-//use bcrypt for password.
-
-export async function PUT(req: Request) {
+export async function PUT(req: NextRequest) {
     const body = await req.json();
     const token = body.cookies.get("accessToken");
     try {
-
         await dbConnect();
         const userData = await verifyJwt(token);
-        console.log("userDATA",userData)
-        //const user = Users.find({userData})
-
-
+        console.log("userDATA", userData)
+        if (userData.data) {
+            const userId = userData.data.userId;
+            const salt: string = await bcrypt.genSalt(10);
+            const hashedPassword: string = await bcrypt.hash(body.newPassword, salt);
+            const user = User.findOneAndUpdate({ _id: userId }, {password: hashedPassword});
+            return NextResponse.json({message: "Password changed!", data: user})
+        }
     } catch (err) {
         console.log(err)
     }
